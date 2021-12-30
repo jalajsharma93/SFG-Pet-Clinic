396187abc38fcbe2155ec8c5aa98d090ef4304ce
jalajsharma93 <jalajsharma92@gmail.com>
2021-12-30 18:05:10 -0500

    Added maven Plugin

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,7 +1,12 @@
+package sfgpetclinic;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@SpringBootConfiguration
 class SfgPetClinicApplicationTests {
 
     @Test
