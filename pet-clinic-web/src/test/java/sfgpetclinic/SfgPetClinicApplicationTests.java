a50311fd841d1967c942d9f7532ba3ce96a3b426
jalajsharma93 <jalajsharma92@gmail.com>
2022-01-05 18:10:22 -0500

    Created map based services for pet vet and owner closes #6

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -3,7 +3,7 @@ package sfgpetclinic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+
 
 @SpringBootTest
 @SpringBootConfiguration
