5383b19baf0430414939f07139186405ebf07748
jalajsharma93 <jalajsharma92@gmail.com>
2022-02-05 15:19:53 -0500

    Created index page and configure controller for listing all Vets closes #16

--- pet-clinic-web/src/main/java/spring/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/spring/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package spring.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import spring.sfgpetclinic.services.VetService;
 
 @Controller
+@RequestMapping("/vets")
 public class VetController {
 
-    @RequestMapping({"/vets","/vets/index","vets/index.html"})
-    public String listVets(){
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/","/index","/index.html",""})
+    public String listVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "Vets/index";
     }
 }
